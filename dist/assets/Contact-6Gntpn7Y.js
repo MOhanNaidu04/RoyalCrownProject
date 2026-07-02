4d5584b2235e5566c119dd4719f365d6854bdfc6
MOhanNaidu04 <chintanipplamohan549@email.com>
2026-07-02 15:52:43 +0530

    Version10

--- dist/assets/Contact-6Gntpn7Y.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./vendor-framer-CToAmdCj.js";import"./vendor-react-0G06IxQq.js";import"./index-BVpvSFYO.js";import{C as r}from"./Contact-DCL0bAgz.js";import"./vendor-gsap-SFc2wnMY.js";import"./vendor-swiper-CLjMzi4o.js";function c(){return t.jsx("div",{className:"rcss-page rcss-contact-page",children:t.jsx(r,{})})}export{c as Contact,c as default};
